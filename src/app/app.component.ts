aff5b20ffe37af2fde013d144eb12f244ce40494
Satendra <satendra.external@credable.in>
2026-03-15 15:37:21 +0530

    All move to production

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,24 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileComponent } from "./components/profile/profile.component";
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, ProfileComponent],
+  imports: [
+    RouterOutlet, CommonModule, ProfileComponent,
+    MatButtonModule, MatMenuModule, MatIconModule,
+  MatBadgeModule,MatSlideToggleModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+   checked = false;
   title = 'satendra-rajput';
 }
--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 
 interface RepositoryModel {
   title: string;
@@ -15,52 +19,68 @@ interface RepositoryModel {
 @Component({
   selector: 'app-overview',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, DragDropModule, MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss'
 })
 export class OverviewComponent {
+  editMode = false; // customize toggle
+  repositories: RepositoryModel[] = [
+    {
+      title: 'ADCB Loan Origination System',
+      visibility: 'Private',
+      description: 'Built configurable multi-step loan journeys in Angular integrated with Java Spring Boot APIs for end-to-end loan onboarding.',
+      platform: 'Angular, Angular Material, Jira, Git, GitHub, Scrum',
+      stars: 120,
+      forks: 35,
+      image: 'https://me.satendracoder.com/assets/images/projects/redmil_fintech/login.png',
+      color: '#dd0031'
+    },
+    {
+      title: 'Axis Bank Dealer LOS',
+      visibility: 'Private',
+      description: 'Developed dealer-focused loan origination flows, dashboards, and application tracking modules in Angular with backend integration.',
+      platform: 'Angular, Angular Material, Jira, Spring Boot, MySQL',
+      stars: 95,
+      forks: 22,
+      image: 'assets/projects/recharge.png',
+      color: '#dd0031'
+    },
+    {
+      title: 'Redmil FinTech Platform',
+      visibility: 'Private',
+      description: 'Developed a full-featured fintech web application supporting AEPS, Recharge, Bill Payments, Wallet Transactions, and Micro ATM services..',
+      platform: ' Angular, Angular Material, REST APIs, JWT, RxJS',
+      stars: 70,
+      forks: 18,
+      image: 'assets/projects/portfolio.png',
+      color: '#dd0031'
+    },
+    {
+      title: 'Redmil PPF (Paint Protection Film)',
+      visibility: 'Private',
+      description: 'Developed a dynamic Angular website for paint protection film services including car listings, product management, blogs, and admin dashboard.',
+      platform: 'Angular, Angular Material, REST APIs, JWT, RxJS, GitHub, Postman',
+      stars: 150,
+      forks: 40,
+      image: 'assets/projects/ui-library.png',
+      color: '#dd0031'
+    }
+  ];
 
- repositories: RepositoryModel[] = [
-{
-title: 'Fintech AEPS System',
-visibility: 'Public',
-description: 'Angular + Spring Boot based AEPS transaction system',
-platform: 'Angular',
-stars: 120,
-forks: 35,
-image: 'https://me.satendracoder.com/assets/images/projects/redmil_fintech/login.png',
-color: '#dd0031'
-},
-{
-title: 'Recharge & Bill Payment Platform',
-visibility: 'Public',
-description: 'Multi recharge platform with payment gateway',
-platform: 'Angular',
-stars: 95,
-forks: 22,
-image: 'assets/projects/recharge.png',
-color: '#dd0031'
-},
-{
-title: 'Developer Portfolio',
-visibility: 'Public',
-description: 'Modern animated portfolio using Angular',
-platform: 'Angular',
-stars: 70,
-forks: 18,
-image: 'assets/projects/portfolio.png',
-color: '#dd0031'
-},
-{
-title: 'Angular UI Component Library',
-visibility: 'Public',
-description: 'Reusable Angular UI components library',
-platform: 'Angular',
-stars: 150,
-forks: 40,
-image: 'assets/projects/ui-library.png',
-color: '#dd0031'
-}
-];
+  constructor() { 
+     const saved = localStorage.getItem('pinnedRepos');
+    if (saved) {
+      this.repositories = JSON.parse(saved);
+    }
+  }
+
+  ngOnInit() {
+
+  }
+  drop(event: CdkDragDrop<RepositoryModel[]>) {
+    moveItemInArray(this.repositories, event.previousIndex, event.currentIndex);
+
+    localStorage.setItem('pinnedRepos', JSON.stringify(this.repositories));
+  }
 }
